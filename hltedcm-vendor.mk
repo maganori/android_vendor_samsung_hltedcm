907573f56133431f2bae3d5d7a684676bd0586bc
yasu-hide <5038337+yasu-hide@users.noreply.github.com>
2018-06-23 01:12:38 +0900

    hltedcm: felica: Separated Makefile of FeliCa proprietary files
    Change-Id: I18a5b72895e7be8b530e567646bd1d23639c0eea

--- hltedcm-vendor.mk
+++ hltedcm-vendor.mk
@@ -373,21 +373,24 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/samsung/hltedcm/proprietary/felica/bin/mfdp:system/bin/mfdp \
     vendor/samsung/hltedcm/proprietary/felica/bin/mfsc:system/bin/mfsc \
-    vendor/samsung/hltedcm/proprietary/felica/etc/osaifu/osaifu.cfg:system/etc/osaifu/osaifu.cfg \
-    vendor/samsung/hltedcm/proprietary/felica/lib/libcordon.so:system/vendor/lib/libcordon.so
+    vendor/samsung/hltedcm/proprietary/felica/etc/osaifu/osaifu.cfg:system/etc/osaifu/osaifu.cfg
  ## snfc
 PRODUCT_COPY_FILES += \
     vendor/samsung/hltedcm/proprietary/snfc/etc/snfc/auto_polling.cfg:system/etc/snfc/auto_polling.cfg \
-    vendor/samsung/hltedcm/proprietary/snfc/etc/snfc/rf.cfg:system/etc/snfc/rf.cfg \
-    vendor/samsung/hltedcm/proprietary/snfc/lib/libnfc_sony.so:system/vendor/lib/libnfc_sony.so \
-    vendor/samsung/hltedcm/proprietary/snfc/lib/libnfc_sony_jni.so:system/vendor/lib/libnfc_sony_jni.so \
-    vendor/samsung/hltedcm/proprietary/snfc/lib/libnfc_sony_ndef.so:system/vendor/lib/libnfc_sony_ndef.so
+    vendor/samsung/hltedcm/proprietary/snfc/etc/snfc/rf.cfg:system/etc/snfc/rf.cfg
+
+PRODUCT_PACKAGES += \
+    libnfc_sony_jni \
+    libnfc_sony \
+    libnfc_sony_ndef
+
  PRODUCT_PACKAGES += \
     MobileFeliCaClient \
     MobileFeliCaMenuApp \
     MobileFeliCaSettingApp \
     MobileFeliCaWebPluginBoot \
-    FeliCaLock
+    FeliCaLock \
+    libcordon
 
 PRODUCT_PACKAGES += \
     SmartcardService \
